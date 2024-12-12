9ba2aa6a83fd4ae5116b08f14d34f5548936c4cf
Anton Kraev <antonkraeww@gmail.com>
2024-12-13 02:26:28 +0300

    all models moved to single file

--- models/models.go
+++ models/models.go
@@ -4,10 +4,15 @@ type ID string
 
 type Status string
 
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type Link string
+
 type Payment struct {
 	Price  uint   `json:"price"`
 	Status Status `json:"status"`
 	User   User   `json:"user"`
 }
-
-type Link string
